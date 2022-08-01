39e4c1c7a14b7b0557f72a3c395ac6236d75001c
Maxime Piscaglia <maxime.piscaglia@mairie-nancy.fr>
2022-08-01 15:01:37 +0200

    selection visible on bottom bar

--- lib/widgets/main_bottom_app_bar.dart
+++ lib/widgets/main_bottom_app_bar.dart
@@ -5,21 +5,31 @@ import 'package:provider/provider.dart';
 import 'package:nancy_stationnement/services/gny_parking.dart';
 import 'package:nancy_stationnement/services/jcdecaux_velostan.dart';
 
-class MainBottomAppBar extends StatelessWidget {
+class MainBottomAppBar extends StatefulWidget {
   const MainBottomAppBar({Key? key, required this.onUpdateTap
       // required this.gny,
       })
       : super(key: key);
 
-  // final GnyParking Function(BuildContext context, {bool listen}) gny;
-  // Providers
-  final gny = Provider.of<GnyParking>;
-  final bikeStations = Provider.of<JcdecauxVelostan>;
-
   final Function onUpdateTap;
 
   @override
+  State<MainBottomAppBar> createState() => _MainBottomAppBarState();
+}
+
+class _MainBottomAppBarState extends State<MainBottomAppBar> {
+  // final GnyParking Function(BuildContext context, {bool listen}) gny;
+  final gny = Provider.of<GnyParking>;
+
+  final bikeStations = Provider.of<JcdecauxVelostan>;
+
+  String selectedButton = "parkings";
+  bool selectedRefresh = false;
+
+  @override
   Widget build(BuildContext context) {
+
+
     final snackBarParking = SnackBar(
       content: Text("Données des Parkings mis à jour (dev mode)"),
       backgroundColor: Colors.blue,
@@ -46,47 +56,74 @@ class MainBottomAppBar extends StatelessWidget {
                 }
               },
               icon: const Icon(Icons.directions_bus)),
-          IconButton(
-              onPressed: () {
-                if (kDebugMode) {
-                  print("Velo button pressed");
-                }
-                onUpdateTap("bikeStations");
+          Container(
+            color: selectedButton == "bikestation" ? 
+            Color.fromARGB(255, 168, 207, 169) : 
+            Color.fromARGB(255, 92, 212, 92),
+            child: IconButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print("Velo button pressed");
+                  }
+                  setState(() {
+                    selectedButton = "bikestation";
+                  });
+                  widget.onUpdateTap("bikeStations");
+          
+                  
+                },
+                icon: const Icon(Icons.directions_bike)),
+          ),
+          Container(
+            color: selectedButton == "parkings" ? 
+              Color.fromARGB(255, 168, 207, 169) : 
+              Color.fromARGB(255, 92, 212, 92),
+            child: IconButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print("parking button pressed");
+                  }
+                  setState(() {
+                    selectedButton = "parkings";
+                  });
+                  widget.onUpdateTap("parkings");
+                  // gny(context, listen: false)
+                  //     .reInitParkingAndGenerateMarkers()
+                  //     .then((value) => {
+                  //           print("reInit Parking from P button"),
+                  //           onUpdateTap("parkings"),
+                  //           ScaffoldMessenger.of(context).showSnackBar(snackBarParking)
+                  //         });
+                },
+                icon: const Icon(Icons.local_parking)),
+          ),
+          Container(
+            color: selectedRefresh ? 
+              Color.fromARGB(255, 168, 207, 169) : 
+              Color.fromARGB(255, 92, 212, 92),
+            child: IconButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print("MAJ Parkings");
+                  }
+                  setState(() {
+                    selectedRefresh = true;
+                  });
+                  gny(context, listen: false).fetchDynamicDataParkings().then(
+                      (value) => {
+                            print("fetchDynamicData from MAJ button"),
+                            widget.onUpdateTap("parkings"),
+                            setState(() {
+                              selectedRefresh = false;
+                            })
+                  });
 
-                
-              },
-              icon: const Icon(Icons.directions_bike)),
-          IconButton(
-              onPressed: () {
-                if (kDebugMode) {
-                  print("parking button pressed");
-                }
-                onUpdateTap("parkings");
-                // gny(context, listen: false)
-                //     .reInitParkingAndGenerateMarkers()
-                //     .then((value) => {
-                //           print("reInit Parking from P button"),
-                //           onUpdateTap("parkings"),
-                //           ScaffoldMessenger.of(context).showSnackBar(snackBarParking)
-                //         });
-              },
-              icon: const Icon(Icons.local_parking)),
-          IconButton(
-              onPressed: () {
-                if (kDebugMode) {
-                  print("MAJ Parkings");
-                }
-                gny(context, listen: false).fetchDynamicDataParkings().then(
-                    (value) => {
-                          print("fetchDynamicData from MAJ button"),
-                          onUpdateTap("parkings")
-                });
-
-                //? Si utilisé, afficher si not null, vider la variable après affichage
-                //? ou créer un tableau. -> gestion d'erreur flutter ? équivalent slim: regarder
-                // ScaffoldMessenger.of(context).showSnackBar(gny(context, listen: false).snackBarError);
-              },
-              icon: const Icon(Icons.update)),
+                  //? Si utilisé, afficher si not null, vider la variable après affichage
+                  //? ou créer un tableau. -> gestion d'erreur flutter ? équivalent slim: regarder
+                  // ScaffoldMessenger.of(context).showSnackBar(gny(context, listen: false).snackBarError);
+                },
+                icon: const Icon(Icons.update)),
+          ),
         ],
       ),
     );
